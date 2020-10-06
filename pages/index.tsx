6ea4e813b601e4a653773574d61f08ed4b151451
Nikolay <chernuh.1994@gmail.com>
2020-10-06 12:16:10 +0300

    Added Tailwind CSS.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,7 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Test</title>
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Test Project
-        </h1>
-      </main>
+    <div className='md:flex bg-white rounded-lg p-24 justify-center'>
+      <h1 className='mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900'> Test Project </h1>
     </div>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    layers: ['utilities'],
+    content: [
+      './pages/**/*.tsx',
+      './components/**/*.tsx'
+    ],
+  },
+}
