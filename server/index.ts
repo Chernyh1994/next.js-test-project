4a517ad30f8bfe97170454721be372877058c822
Nikolay <chernuh.1994@gmail.com>
2020-10-06 14:55:22 +0300

    Added docker and mongoose

--- server/index.ts
+++ server/index.ts
@@ -27,23 +27,3 @@ app.prepare().then(() => {
   console.error(error);
   process.exit(1);
 });
-
-// (async () => {
-//   try {
-//     await app.prepare();
-//     const server = express();
-//
-//     server.all("*", (req: Request, res: Response) => {
-//       return handle(req, res);
-//     });
-//
-//     server.listen(port, (err?: any) => {
-//       if (err) throw err;
-//       console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
-//     });
-//
-//   } catch (e) {
-//     console.error(e);
-//     process.exit(1);
-//   }
-// })();
